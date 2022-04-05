9354f2b64a1a5bb2c7206cb9844cac0ec77dea15
nabilaba <nabilazizbimaanggita@gmail.com>
2022-04-05 11:30:04 +0700

    Update Calculator

--- source/dom/calculator-cp/index.js
+++ source/dom/calculator-cp/index.js
@@ -1,14 +1,40 @@
 //handle when the number is pressed. It renders the number into #input element
 function number(number) {
     // TODO: answer here
+    if (document.getElementById("input").value === "0") {
+        document.getElementById("input").value = number;
+    } else {
+        document.getElementById("input").value += number;
+    }
 }
 
 //handle when the Del button, AC button and other operators (+, /, -, x) are pressed. It renders the operator into #input element
 function operator(operator) {
     // TODO: answer here
+    if (operator === "+") {
+        document.getElementById("input").value += "+";
+    } else if (operator === "-") {
+        document.getElementById("input").value += "-";
+    } else if (operator === "*") {
+        document.getElementById("input").value += "*";
+    } else if (operator === "/") {
+        document.getElementById("input").value += "/";
+    } else if (operator === "AC") {
+        document.getElementById("input").value = "";
+    } else if (operator === "Del") {
+        document.getElementById("input").value = document.getElementById("input").value.slice(0, -1);
+    } else if (operator === ".") {
+        document.getElementById("input").value += ".";
+    }
 }
 
 //handle when = button is pressed. It renders the result into #input element
 function calculate() {
     // TODO: answer here
+    try {
+        document.getElementById("input").value = eval(document.getElementById("input").value);
+    } catch (e) {
+        document.getElementById("input").value = "0" + document.getElementById("input").value + "0";
+        document.getElementById("input").value = eval(document.getElementById("input").value);
+    }
 }
